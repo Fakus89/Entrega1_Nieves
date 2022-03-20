9aabac0a1d49d4094b294c4c3d38e80fcf3d3a89
Facundo Nieves <facundo.nieves@hotmail.com>
2022-03-20 00:15:32 -0300

    formulario3

--- taller/forms.py
+++ taller/forms.py
@@ -3,21 +3,21 @@ from functools import partial
 from django import forms
 
 
-class Cliente(forms.Form):
+class FormCliente(forms.Form):
     nombre = forms.CharField(max_length=20)
     apellido = forms.CharField(max_length=25)
     telefono = forms.IntegerField()
     email = forms.EmailField()
 
 
-class Auto(forms.Form):
+class FormAuto(forms.Form):
     marca =forms.CharField(max_length=15)
     modelo =forms.CharField(max_length=25)
     patente =forms.CharField(max_length=7)
     
 
 
-class Problema(forms.Form):
+class FormProblema(forms.Form):
     inconveniente = forms.CharField(max_length=50)
     ingreso = forms.DateTimeField()
     arreglado = forms.BooleanField()
--- taller/models.py
+++ taller/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Cliente(models.Model):
     nombre = models.CharField(max_length=20)
     apellido = models.CharField(max_length=25)
-    telefono = models.IntegerField(max_length=11)
+    telefono = models.IntegerField()
     email = models.EmailField()
 
 
